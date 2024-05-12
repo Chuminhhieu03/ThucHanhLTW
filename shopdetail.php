661b67809039f39f69e4d0116ad78eaab5d0f55a
chuhieu03 <chuhieuqb2003@gmail.com>
2024-05-12 11:13:35 +0700

    Hieu

---
+++ shopdetail.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Apple</title>
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="responsive.css">
+      <!-- <link rel="stylesheet" href="reset.css"> buat reset css -->
+      <link rel="shortcut icon" type="image/jpg" href="./img/apple-logo.png">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"
+         crossorigin="anonymous" />
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+   </head>
+   <body>
+      <!-- Bagian header-->
+      <header>
+         <div class="header-wrapper">
+            <span class="menu-logo hp-viewport fas fa-ellipsis-h"></span>
+            <ul class="header-list">
+               <li class="header-content hp-viewport apple-header"><a href="https://www.apple.com/"><img class="logo"
+                  src="./img/apple-logo.png"></a></li>
+               <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/mac/">Mac</a></li>
+               <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/ipad/">iPad</a></li>
+               <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/iphone/">iPhone</a></li>
+               <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/watch/">Watch</a></li>
+               <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/tv/">TV</a></li>
+               <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/music/">Music</a></li>
+               <li class="header-content hp-viewport-hide"><a href="https://support.apple.com/">Support</a></li>
+               <!-- masukkin input search -->
+               <li>
+                  <div class="search search-container">
+                     <div class="search-icon" id="searchIcon"><i class="fas fa-search"></i></div>
+                     <div class="search-dropdown" id="searchDropdown">
+                        <input type="text" placeholder="Search...">
+                        <button>Search</button>
+                     </div>
+                  </div>
+               </li>
+               <!-- masukkin input search -->
+               <li class="header-content hp-viewport header-shoping"><a href="https://www.apple.com/us/shop/goto/bag"
+                  class="fas fa-shopping-bag"></a></li>
+            </ul>
+         </div>
+      </header>
+      <main>
+         <!-- Bagian content atas -->
+         <div class="section-shop">
+            <div class="title-shop">
+                <h1>Shop Iphone</h1>
+             </div>
+
+             <ul class="shop-list">
+                <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/mac/">Mac</a></li>
+                <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/ipad/">iPad</a></li>
+                <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/iphone/">iPhone</a></li>
+                <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/watch/">Watch</a></li>
+                <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/tv/">TV</a></li>
+                <li class="header-content hp-viewport-hide"><a href="https://www.apple.com/music/">Music</a></li>
+                <li class="header-content hp-viewport-hide"><a href="https://support.apple.com/">Support</a></li>
+            </ul>
+
+            <div class="title-shop-detail">
+                <h1>All model</h1>
+             </div>
+
+             <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                   <div class="swiper-slide swiper-slide-shop">
+                      <p class="title-swiper">Iphone15</p>
+                      <button>Buy</button>
+                      <p> From $999</p>
+                      <img src="img/iphone_12.jpg" alt="">
+                   </div>
+                   <div class="swiper-slide swiper-slide-shop">
+                      <p class="title-swiper">Iphone15</p>
+                      <button>Buy</button>
+                      <p> From $999</p>
+                      <img src="img/iphone_12.jpg" alt="">
+                   </div>
+                   <div class="swiper-slide swiper-slide-shop">
+                      <p class="title-swiper"> Iphone15</p>
+                      <button>Buy</button>
+                      <p> From $999</p>
+                      <img src="img/iphone-15.png" alt="">
+                   </div>
+                   <div class="swiper-slide swiper-slide-shop">
+                      <p class="title-swiper">Iphone15</p>
+                      <button>Buy</button>
+                      <p> From $999</p>
+                      <img src="img/macbook-air.png" alt="">
+                   </div>
+                </div>
+                <div class="swiper-pagination"></div>
+             </div>
+        </div>
+      </main>
+      <!-- Bagian footer-->
+      <footer>
+         <div class="footer-container">
+            <div class="foot-notes">
+               <ul>
+                  <li>
+                     1. Price includes a $30 AT&T, T-Mobile, Sprint, or Verizon discount. Requires activation with
+                     carrier. Monthly pricing: Available to qualified customers and requires 0% APR.
+                  </li>
+                  <li>
+                     2. Monthly pricing: Available to qualified customers and requires 0% APR, 24-month installment
+                     loan with Citizens One or Apple Card Monthly Installments.
+                  </li>
+                  <li>
+                     3. $9.99/month after free trial. No commitment. Plan automatically renews after trial until
+                     cancelled.
+                  </li>
+                  <li>
+                     4. The Apple One free trial includes only services that you are not currently using through a
+                     free trial or a subscription.
+                  <li>
+                  <li>
+                     To access and use all the features of Apple Card, you must add Apple Card to Wallet on an iPhone
+                     or iPad with iOS 12.4 or later or iPadOS.
+                  </li>
+                  <li>
+                     Available for qualifying applicants in the United States.
+                  </li>
+                  <li>
+                     Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City Branch.
+                  </li>
+                  <li>
+                     Apple TV+ is $4.99/month after free trial. One subscription per Family Sharing group. Offer good
+                     for 3 months after eligible device activation. Plan automatically renews until cancelled.
+                     Restrictions and other terms apply.
+                  </li>
+               </ul>
+            </div>
+         </div>
+         </div>
+         <hr style="width: 70%;">
+         <div class="footer-container">
+            <div class="footer-list">
+               <h3>Shop and Learn</h3>
+               <p><a href="https://www.apple.com/mac">Mac</a></p>
+               <p><a href="https://www.apple.com/ipad">iPad</a></p>
+               <p><a href="https://www.apple.com/iphone">iPhone</a></p>
+               <p><a href="https://www.apple.com/watch">Watch</a></p>
+               <p><a href="https://www.apple.com/tv">TV</a></p>
+               <p><a href="https://www.apple.com/music">Music</a></p>
+               <p><a href="https://www.apple.com/airpods">AirPods</a></p>
+               <p><a href="https://www.apple.com/homepod">HomePod</a></p>
+               <p><a href="https://www.apple.com/ipad-touch">iPod touch</a></p>
+               <p><a href="https://www.apple.com/shop/accessories/all">Accessories</a></p>
+               <p><a href="https://www.apple.com/shop/gift-cards">Gift Cards</a></p>
+            </div>
+            <div class="footer-list">
+               <h3>Services</h3>
+               <p><a href="https://www.apple.com/apple-music">Apple Music</a></p>
+               <p><a href="https://www.apple.com/apple-tv-plus">Apple TV+</a></p>
+               <p><a href="https://www.apple.com/apple-fitnes-plus">Apple Fitness+</a></p>
+               <p><a href="https://www.apple.com/apple-news">Apple News+</a></p>
+               <p><a href="https://www.apple.com/apple-arcade">Apple Arcade</a></p>
+               <p><a href="https://www.apple.com/icloud">iCloud</a></p>
+               <p><a href="https://www.apple.com/apple-one">Apple One</a></p>
+               <p><a href="https://www.apple.com/apple-card">Apple Card</a></p>
+               <p><a href="https://www.apple.com/apple-books">Apple Books</a></p>
+               <p><a href="https://www.apple.com/app-store">App Store</a></p>
+            </div>
+            <div class="footer-list">
+               <h3>Apple Store</h3>
+               <p><a href="https://www.apple.com/retail">Find a Store</a></p>
+               <p><a href="https://www.apple.com/shop">Shop Online</a></p>
+               <p><a href="https://www.apple.com/retail/geniusbar">Genius Bar</a></p>
+               <p><a href="https://www.apple.com/today">Today at Apple</a></p>
+               <p><a href="https://www.apple.com/today/camp">Apple Camp</a></p>
+               <p><a href="https://apps.apple.com/app/apple-store/id375380948">Apple Store App</a></p>
+               <p><a href="https://www.apple.com/shop/refurbished">Refurbished and Clearance</a></p>
+               <p><a href="https://www.apple.com/shop/browse/financing">Financing</a></p>
+               <p><a href="https://www.apple.com/shop/trade-in">Apple Trade In</a></p>
+               <p><a
+                  href="https://secure4.store.apple.com/shop/signIn/orders?r=SCKFUHKXACXX7DYHYT9JT7JJTAPAXHFKH&s=aHR0cHM6Ly9zZWN1cmU0LnN0b3JlLmFwcGxlLmNvbS9zaG9wL29yZGVyL2xpc3R8MWFvc2MyMzE1ZDYzNTE4MGIwZTg4MWE3YzIwOWJjMTVkMjNlMzI4ZTBkN2Y">Order
+                  Status</a>
+               </p>
+               <p><a href="https://www.apple.com/shop/help">Shopping Help</a></p>
+            </div>
+            <!-- <div class="footer-copyright">
+               <img class="logo-footer" src="img/apple-logo.png" alt="footer logo">
+               <p class="txt-copy"><a target="_blank" href="https://www.apple.com/id/">appleindonesia</a> &copy; 2021</p>
+               </div> -->
+         </div>
+      </footer>
+      <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+      <script src="script.js"></script>
+      <script src="swiper_script_shop.js"></script>
+   </body>
+</html>
